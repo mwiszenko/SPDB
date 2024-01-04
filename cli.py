1488e6e9ba5bb3f90c3e848f5a5f910e164e4cf2
Michał Wiszenko <wiszenkom@gmail.com>
2024-01-04 23:59:49 +0100

    Add simple cli template

---
+++ cli.py
@@ -0,0 +1,30 @@
+import argparse
+
+
+class ModeMapper:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def run(args) -> None:
+        pass
+
+    @staticmethod
+    def test(args) -> None:
+        pass
+
+
+if __name__ == '__main__': # pragma: no cover
+    parser = argparse.ArgumentParser()
+    modes = parser.add_subparsers(dest="command", required=True)
+
+    # run mode
+    run_mode = modes.add_parser("run")
+    run_mode.set_defaults(func=ModeMapper.run)
+
+    # test mode
+    test_mode = modes.add_parser("test")
+    test_mode.set_defaults(func=ModeMapper.test)
+
+    arguments = parser.parse_args()
+    arguments.func(arguments)
