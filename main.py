7280576d9f1a6e35fd6bd1fff43111b9176436fb
Michał Wiszenko <wiszenkom@gmail.com>
2024-01-13 23:07:15 +0100

    Modify cli arguments

--- main.py
+++ main.py
@@ -65,15 +65,15 @@ if __name__ == "__main__":  # pragma: no cover
     # analysis mode
     analysis_mode = modes.add_parser("analyse")
     analysis_mode.set_defaults(func=ModeMapper.analyse)
-    analysis_mode.add_argument("-k", "--vertices", type=vertex_number, default=10)
+    analysis_mode.add_argument("-v", "--vertices", type=vertex_number, default=10)
     analysis_mode.add_argument(
-        "-v", "--vertex_increment", type=non_negative_int, default=10
+        "-vi", "--vertex_increment", type=non_negative_int, default=10
     )
-    analysis_mode.add_argument("-n", "--shapes", type=positive_int, default=10)
+    analysis_mode.add_argument("-s", "--shapes", type=positive_int, default=10)
     analysis_mode.add_argument(
-        "-c", "--shape_increment", type=non_negative_int, default=10
+        "-si", "--shape_increment", type=non_negative_int, default=10
     )
-    analysis_mode.add_argument("-i", "--iterations", type=positive_int, default=5)
+    analysis_mode.add_argument("-it", "--iterations", type=positive_int, default=100)
     analysis_mode.add_argument("-o", "--output_dir", type=str, default="output")
 
     arguments = parser.parse_args()
